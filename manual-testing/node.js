030378e570d3346ff52932605c71cb7739ee971d
Nicklas Gummesson <nicklas.gummesson@gmail.com>
2018-04-01 12:10:42 -0700

    DRY test files

--- manual-testing/node.js
+++ manual-testing/node.js
@@ -1,28 +1,14 @@
-require("../index").install();
+const exampleDomFixture = require('./example-dom-fixture');
+
+const jsdomDevtoolsFormatter = require("../index")
+jsdomDevtoolsFormatter.install();
 
 const { JSDOM } = require("jsdom");
-const dom = new JSDOM(`<!DOCTYPE html>
-  <div class="testing class names" id="root" data-foo-bar="baz">
-    <div class="first">
-      something else
-    </div>
-    <!-- a comment -->
-    <div class="second">
-      <div class="second-inner">
-        something something
-      </div>
-    </div>
-    <p>
-      <span class="example">Hello world</span>
-    </p>
-
-    and some text outside elements
-  </div>
-`);
+const dom = new JSDOM(`<!DOCTYPE html>${exampleDomFixture}`);
 const { window } = dom;
 const { document } = window;
 
-let a = document.getElementById("root");
+let exampleNode = document.getElementById("root");
 debugger;
-// type a<enter> in the console,
+// type exampleNode<enter> in the console,
 // it should be rendered like an actual HTML element which can be expanded and explored
